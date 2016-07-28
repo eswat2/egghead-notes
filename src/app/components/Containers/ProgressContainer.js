8a8b776772db0025517cd2fc311a779f2bcc8607
eswat2 <eswat2@gmail.com>
2016-07-27 22:27:01 -0700

    make the ping bar thinner

--- src/app/components/Containers/ProgressContainer.js
+++ src/app/components/Containers/ProgressContainer.js
@@ -6,8 +6,8 @@ import store from '../../utils/store';
 const ProgressContainer = observer(() => {
   // console.log('-- render:  ProgressContainer');
   return (
-    <div className="progress" style={{height:5,borderRadius:0}}>
-      <div className="progress-bar progress-bar-info progress-bar-striped" role="progressbar" style={{width: store.data.kounter + '%'}}>
+    <div className="progress" style={{height:2,borderRadius:0}}>
+      <div className="progress-bar progress-bar-info" role="progressbar" style={{width: store.data.kounter + '%'}}>
       </div>
     </div>
   )
